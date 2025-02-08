edc41f38918bdac8188ff181244208d984b896e8
NemetKetoleu <nemetfrejus1@gmail.com>
2025-02-07 20:13:27 -0700

    Add project files.

---
+++ mission5/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mission5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult price()
+        {
+            return View();
+        }
+    }
+}
---
+++ mission5/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#calculateBtn').click(function () {
+        var hours = $('#hours').val();
+        var hourlyRate = $('#hourly-rate').val();
+
+        if (hours > 0) {
+            var total = hours * hourlyRate;
+            $('#total-output').text('$' + total.toFixed(2));
+        } else {
+            alert('Please enter a positive number of hours.');
+        }
+    });
+});
